9d226cbcda15cce24ca5b808ad6da6d530e4f2ed
sumitsharansatsangi <sharansumitkumar@gmail.com>
2023-12-03 08:38:32 +0530

    code updated

--- test/secure_window_test.dart
+++ test/secure_window_test.dart
@@ -7,13 +7,13 @@ void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (message) async => '42');
   });
 
   tearDown(() {
-    channel.setMockMethodCallHandler(null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (message) async => null);
   });
 
   test('getPlatformVersion', () async {});
